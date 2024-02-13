ab73cbed8d4d138361a0235281e87bb827bf1166
ren <denis30510@gmail.com>
2024-02-13 16:35:55 +0300

    created search method

--- service/search.py
+++ service/search.py
@@ -1,10 +1,16 @@
+import re
 from abc import ABC, abstractmethod
+from bs4 import BeautifulSoup
+import httpx
 
 
 class Domain(ABC):
 
-    def __init__(self, domain):
-        self.domain = domain
+    url = 'https://who.is/whois/'
+
+    @abstractmethod
+    def __init__(self, name):
+        pass
 
     @abstractmethod
     def search(self, name):
@@ -17,11 +23,22 @@ class Domain(ABC):
 
 class DomainRU(Domain):
 
-    def search(self, name):
-        pass
+    def __init__(self, name):
+        self.name = name
+
+    def search(self):
+        data = httpx.get(self.url + self.name, timeout=200.0).text
+        if '504 Gateway' in data:
+            return None
+        soup = BeautifulSoup(data, 'lxml').find(class_='col-md-12 queryResponseBodyValue').text
+        text_search = re.findall(r'paid-till:     \d{4}-\d\d-\d\d', soup)[0][14:]
+        return text_search
 
     def package(self):
         pass
 
 
 
+x = DomainRU('ya.ru')
+
+print(x.search())
