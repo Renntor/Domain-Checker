b1f97d0641194afb65a3809d8263ecb3f8303ba3
ren <denis30510@gmail.com>
2024-02-15 16:10:53 +0300

    search method

--- service/search.py
+++ service/search.py
@@ -9,15 +9,11 @@ class Domain(ABC):
     url = 'https://who.is/whois/'
 
     @abstractmethod
-    def __init__(self, name):
+    def __init__(self):
         pass
 
     @abstractmethod
-    def search(self, name):
-        pass
-
-    @abstractmethod
-    def package(self):
+    def search_data(self):
         pass
 
 
@@ -26,19 +22,14 @@ class DomainRU(Domain):
     def __init__(self, name):
         self.name = name
 
-    def search(self):
+    def search_data(self):
         data = httpx.get(self.url + self.name, timeout=200.0).text
         if '504 Gateway' in data:
             return None
-        soup = BeautifulSoup(data, 'lxml').find(class_='col-md-12 queryResponseBodyValue').text
-        text_search = re.findall(r'paid-till:     \d{4}-\d\d-\d\d', soup)[0][14:]
-        return text_search
-
-    def package(self):
-        pass
+        soup = BeautifulSoup(data, 'lxml').find(style='border:0px;').text
+        experi_date = re.findall(r'paid-till:     \d{4}-\d\d-\d\d', soup)[0][15:]
+        created_date = re.findall(r'created:       \d{4}-\d\d-\d\d', soup)[0][15:]
+        return created_date, experi_date
 
 
 
-x = DomainRU('ya.ru')
-
-print(x.search())
